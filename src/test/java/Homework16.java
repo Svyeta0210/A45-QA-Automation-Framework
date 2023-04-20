a9652e6ff621c4993bd0fb3c9295896150a3859c
Svyeta0210 <son.nam.kim@testpro.io>
2023-04-20 19:52:49 -0400

    corrections in the code

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -5,28 +5,39 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+
 import java.time.Duration;
 
-public class Homework16 {
+public class Homework16 extends BaseTest{
 
- @Test
-public static void registrationNavigation(){
+     @Test
+     public static void registrationNavigation() throws InterruptedException {
 
-     // open Chrome Browser
-     WebDriver driver = new ChromeDriver();
-     //wait 10 seconds for an element to load before failing a test
-     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-     //navigate, opens a URL
-     String url = "https://bbb.testpro.io/";
-     driver.get(url);
-     //click to Registration link
-     WebElement registrationLink = driver.findElement(By.cssSelector("#hel"));
-     registrationLink.click();
-     //Assert Registration page is Displayed
-     WebElement registrationPage = driver.findElement(By.xpath(("//div[@class='login-wrapper']")));
-     Assert.assertTrue(registrationPage.isDisplayed());
-     //close the browser
-     driver.quit();
+          //      Added ChromeOptions argument below to fix websocket error
+          ChromeOptions options = new ChromeOptions();
+          options.addArguments("--remote-allow-origins=*");
+
+
+          // open Chrome Browser
+          WebDriver driver = new ChromeDriver(options);
+          //wait 10 seconds for an element to load before failing a test
+          driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+          //navigate, opens a URL
+          String url = "https://bbb.testpro.io/";
+          driver.get(url);
+          //click to Registration link
+          WebElement registrationLink = driver.findElement(By.cssSelector("a[id='hel']"));
+          registrationLink.click();
+          //Assert Registration page is Displayed
+          String registrationUrl = "https://bbb.testpro.io/registration.php";
+          Assert.assertEquals(driver.getCurrentUrl(), registrationUrl);
+
+          WebElement registrationPage = driver.findElement(By.cssSelector("div[class='login-wrapper']"));
+          Assert.assertTrue(registrationPage.isDisplayed());
+          Thread.sleep(3000);
+          //close the browser
+          driver.quit();
 
  }
 
